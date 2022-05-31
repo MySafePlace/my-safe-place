758f23136daa915e9a91e78354075c88b1413fda
Marco Antonio Manrique Acha <63822742+MarcoMnrq@users.noreply.github.com>
2022-05-30 19:11:50 -0500

    feat: added new sections

--- components/PageFooter.vue
+++ components/PageFooter.vue
@@ -8,24 +8,24 @@
       >
         <svg
           xmlns="http://www.w3.org/2000/svg"
-          fill="none"
-          stroke="currentColor"
-          stroke-linecap="round"
-          stroke-linejoin="round"
-          stroke-width="2"
           class="w-10 h-10 text-white p-2 bg-indigo-500 rounded-full"
+          fill="none"
           viewBox="0 0 24 24"
+          stroke="currentColor"
+          stroke-width="2"
         >
           <path
-            d="M12 2L2 7l10 5 10-5-10-5zM2 17l10 5 10-5M2 12l10 5 10-5"
-          ></path>
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            d="M4.318 6.318a4.5 4.5 0 000 6.364L12 20.364l7.682-7.682a4.5 4.5 0 00-6.364-6.364L12 7.636l-1.318-1.318a4.5 4.5 0 00-6.364 0z"
+          />
         </svg>
-        <span class="ml-3 text-xl">Tailblocks</span>
+        <span class="ml-3 text-xl">MySafePlace</span>
       </a>
       <p
         class="text-sm text-gray-500 sm:ml-4 sm:pl-4 sm:border-l-2 sm:border-gray-200 sm:py-2 sm:mt-0 mt-4"
       >
-        © 2020 Tailblocks —
+        © 2022 Tailblocks —
         <a
           href="https://twitter.com/knyttneve"
           class="text-gray-600 ml-1"
---
+++ pages/chat.vue
@@ -0,0 +1,11 @@
+<template>
+  <!-- Replace with your content -->
+  <div class="px-4 py-6 sm:px-0 bg-gray-900 h-screen"></div>
+  <!-- /End replace -->
+</template>
+
+<script>
+export default {
+  name: 'ChatPage',
+}
+</script>

53dd2d4c14647abf0da9cce9221397fcf9888ba2
Marco Antonio Manrique Acha <63822742+MarcoMnrq@users.noreply.github.com>
2023-03-24 22:35:16 -0500

    Update default.vue

--- layouts/default.vue
+++ layouts/default.vue
@@ -7,9 +7,9 @@
     <CtaSection/>
     <script>
       window.watsonAssistantChatOptions = {
-        integrationID: "518a9043-05bb-4278-b728-c9663c147b35", // The ID of this integration.
-        region: "us-south", // The region your integration is hosted in.
-        serviceInstanceID: "2966eee3-39a3-4360-b704-e49ca8ab7d53", // The ID of your service instance.
+        integrationID: '518a9043-05bb-4278-b728-c9663c147b35', // The ID of this integration.
+        region: 'us-south', // The region your integration is hosted in.
+        serviceInstanceID: '2966eee3-39a3-4360-b704-e49ca8ab7d53', // The ID of your service instance.
         onLoad: function (instance) {
           instance.writeableElements.beforeInputElement.innerHTML = '¿Estás en una emergencia? Accede a la línea de ayuda 24/7 en el menú desplegable del chat ubicado en la parte superior.';
           instance.updateCustomMenuOptions('bot', [
